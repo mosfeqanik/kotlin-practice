ece27de06f39fb4789a59c623d4906bf0dc4a6cf
mosfeqanik <mosfeqanik01@gmail.com>
2022-10-06 15:39:10 +0600

    kotlin oop added

---
+++ app/src/main/java/com/mosfeqanik/mykotlinapplication/oop/MobilePhone.kt
@@ -0,0 +1,7 @@
+package com.mosfeqanik.mykotlinapplication.oop
+
+class MobilePhone (osName:String, brand:String, model:String) {
+    init {
+        println("OSname :$osName,brand:$brand,model:$model")
+    }
+}
