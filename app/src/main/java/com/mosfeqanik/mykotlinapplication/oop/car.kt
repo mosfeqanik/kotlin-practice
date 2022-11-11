d24c54e051a82c7599436a2af0b25077a858e663
mosfeqanik <mosfeqanik01@gmail.com>
2022-11-11 13:59:49 +0600

    lateinit + private set + get added

--- app/src/main/java/com/mosfeqanik/mykotlinapplication/oop/car.kt
+++ app/src/main/java/com/mosfeqanik/mykotlinapplication/oop/car.kt
@@ -5,30 +5,35 @@ fun main() {
     carDetails.owner
     println(carDetails.owner)
     println("cardetails ${carDetails.myBrand}")
-    carDetails.maxspeed= 200
+    carDetails.maxspeed = 200
     println("cardetails ${carDetails.maxspeed}")
 
-    carDetails.maxspeed= -2
-    println("cardetails ${carDetails.maxspeed}")
-
-
+//    carDetails.maxspeed = -2
+//    println("cardetails ${carDetails.maxspeed}")
+//      carDetails.myModel="m3"
+//      because set is private
+    println("car model is ${carDetails.myModel}")
 }
 
 class car() {
     lateinit var owner: String
 
-    init {
-        owner = "ANIK"
-    }
 
     val myBrand: String = "BMW"
         get() {
             return field.toLowerCase()
         }
     var maxspeed: Int = 250
-//        get() = field
+        //        get() = field
         set(value) {
             field =
                 if (value > 0) value else throw java.lang.IllegalArgumentException("Maxspeed can not be below zero")
         }
+    var myModel: String = "MS"
+        private set
+
+    init {
+        owner = "ANIK"
+        this.myModel = "m3"
+    }
 }
